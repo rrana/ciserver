39bc3344341aa2475d854c3bec61391ca7a9457f
root <root@irobot-OS.(none)>
2012-09-08 23:54:55 +0600

    CI server script updated

--- ci-setup.sh
+++ ci-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e -x
 
+#rana.ete.nsu@gmail.com
+
 #RUN THIS SCRIPT AS ROOT USER
 
 #export DEBIAN_FRONTEND=noninteractive
@@ -11,13 +13,7 @@ apt-get install build-essential -y
 
 ####Jenkins Part ####
 
-#Java installation
-
-#sudo add-apt-repository ppa:ferramroberto/java -y
-#sudo apt-get update
-#sudo apt-get install sun-java6-jre sun-java6-plugin sun-java6-fonts
-
-#sudo apt-get install openjdk-6-jre-headless
+##Java installation
 
 echo deb http://ftp.fr.debian.org/debian/ squeeze non-free >> /etc/apt/sources.list
 echo deb-src http://ftp.fr.debian.org/debian/ squeeze non-free >> /etc/apt/sources.list
@@ -25,22 +21,21 @@ apt-get update
 apt-get install sun-java6-bin -y
 apt-get install sun-java6-jdk -y
 
-#Jenkins Installation
+##Jenkins Installation
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian binary/ >> /etc/apt/source.list
 apt-get update
 apt-get install jenkins -y
 
-#Apache installation
+##Apache installation
 aptitude install libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev libgeoip-dev -y
-
 apt-get install apache2 -y
 
 a2enmod proxy
 a2enmod proxy_http
 a2enmod vhost_alias
 a2dissite default
-
+#Creating apache vhost file for jenkins
 touch /etc/apache2/sites-available/jenkins
 jenkins_vhost="/etc/apache2/sites-available/jenkins"
 jenkins_vhost_content="<VirtualHost *:80>
@@ -59,12 +54,16 @@ jenkins_vhost_content="<VirtualHost *:80>
 echo "Creating jenkins_vhost file..."
 echo "$jenkins_vhost_content" > $jenkins_vhost
 
+#enabling jenkins vhost
 a2ensite jenkins
 
+#restarting apache
 /etc/init.d/apache2 stop
 /etc/init.d/apache2 start
 
-#ANT Installation
+#Jenkins login URL: ci.test.com (as mentioned in apache vhost file. You have to change this according to your domain information) OR http://localhost/
+
+##ANT Installation
 apt-get install ant
 
 ##Installation of Selenium
@@ -75,7 +74,7 @@ apt-get install xvfb
 #Unzip
 apt-get install unzip
 
-#chrome driver
+#chrome driver installation
 cd ~
 wget http://selenium.googlecode.com/files/selenium-server-2.25.0.zip
 unzip selenium-server-2.25.0.zip 
@@ -89,45 +88,44 @@ wget http://chromedriver.googlecode.com/files/chromedriver_linux64_23.0.1240.0.z
 unzip chromedriver_linux64_23.0.1240.0.zip
 cp chromedriver /usr/local/bin/
 chmod a+x /usr/local/bin/chromedriver
-#You can test chrome driver by runnin: chromedriver in your terminal
+#You can test chrome driver by running: chromedriver in your terminal
 
 apt-get install lo-menubar
 #apt-get install ImageMagick
 apt-get install graphviz
 
-apt-get install -y libgconf2-4 libxss1 xdg-utils   #google chrome dependencies
+#google chrome dependencies
+apt-get install -y libgconf2-4 libxss1 xdg-utils   
 cd ~
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #you have to run: chromedriver  before google-chrome installation
-dpkg -i google-chrome-stable_current_amd64.deb
+#Run the following two command manually at the end of this script installation
+#chromedriver
+#dpkg -i google-chrome-stable_current_amd64.deb
 
 
 ####Artifactory Part ####
 
+#Setting environment
+echo JAVA_HOME=/usr/lib/jvm/java-6-sun/ >> /etc/environment
+echo export JAVA_HOME=/usr/lib/jvm/java-6-sun/ >> /etc/profile
+echo export JAVA_HOME=/usr/lib/jvm/java-6-sun/ >> ~/.bash_profile
+source ~/.bash_profile
 
+#For test purpose I am downloading an evaluation version of Artifactory. You have to change this download link after wget with your licensed version's #download link:
+cd ~
+wget https://store.artifactoryonline.com/addons/download?license=32b67fea0faca0b9227d0e61623ac8cdbbcc4711
+mv download\?license\=32b67fea0faca0b9227d0e61623ac8cdbbcc4711 artifactory-powerpack-standalone-2.6.3.zip
+unzip artifactory-powerpack-standalone-2.6.3.zip
+cd artifactory-powerpack-2.6.3/bin/
+./install.sh
 
+echo export JAVA_HOME=/usr/lib/jvm/java-6-sun/jre >> /etc/artifactory/default
 
+## You can check artifactory installation with: service artifactory check
+## active artifactory with: service artifactory start
 
+service artifactory start
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#After successful run your Artifactory login url will be: http://ec2-75-101-213-8.compute-1.amazonaws.com:8081/artifactory/
+#Default Artifactory login information: user/pass is admin/password
